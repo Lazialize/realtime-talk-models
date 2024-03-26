4a1b5001f48daa65bf509e3e22b9907b108fb528
Lazialize <lazinelia@gmail.com>
2024-03-27 01:13:35 +0900

    chore: export entities

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {
+  type Conversation,
+  conversation,
+} from './entities/conversation.entity';
+import { type Message, message } from './entities/message.entity';
+import { type Participant, participant } from './entities/participant.entity';
+import { type User, user } from './entities/user.entity';
+
+export {
+  conversation,
+  message,
+  participant,
+  user,
+  type Conversation,
+  type Message,
+  type Participant,
+  type User,
+};
